fa479079a379a59b8db984e0776b01fae91000d7
nidwinranz <nidwin.ranze@gmail.com>
2025-03-24 14:09:54 +0530

    initial commit

--- next.config.js
+++ next.config.js
@@ -3,8 +3,7 @@ const nextConfig = {
     experimental: {
       adjustFontFallbacks: false,
     },
-
-    target: 'serverless', 
+    
   };
   
   module.exports = nextConfig;
--- src/app/api/s3-upload/route.js
+++ src/app/api/s3-upload/route.js
@@ -21,6 +21,7 @@ async function uploadFileToS3(fileBuffer, fileName, contentType) {
   await s3Client.send(command);  // Upload the file to S3
   return fileName;
 }
+
 export const dynamic = "force-dynamic";
 
 export async function POST(request) {
@@ -34,7 +35,8 @@ export async function POST(request) {
 
     const buffer = Buffer.from(await file.arrayBuffer());
     const contentType = file.type;
-    const fileName = await uploadFileToS3(buffer, file.name, contentType);
+    const fileName = `${Date.now()}-${file.name}`; // Ensure the file name is unique
+    await uploadFileToS3(buffer, fileName, contentType);
 
     const s3BaseUrl = process.env.AWS_S3_BASE_URL;
     const imageUrl = `${s3BaseUrl}/uploads/${fileName}`;
