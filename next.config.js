5c6be1a0461fe7ca38fe3d9eab43efed092f762a
nidwinranz <nidwin.ranze@gmail.com>
2025-04-07 06:02:13 +0530

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true, // Optional if you're using the new app directory in Next.js 13+
-  },
-  distDir: '.output', // Use '.output' for Cloudflare Pages
-  trailingSlash: true, // Optional, but useful for static sites
+  distDir: '.output', // Output directory for Cloudflare Pages
+  trailingSlash: true, // Optional but useful for static sites
 };
 
 module.exports = nextConfig;
